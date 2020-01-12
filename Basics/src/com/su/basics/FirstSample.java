2f675ad9beb8d0874d4bfbaa027906bdeec2d235
SuRongLuo <1253881769@qq.com>
2020-01-12 09:50:22 +0800

    [JAVA 基本类型 变量的声明 变量的初始化]

---
+++ Basics/src/com/su/basics/FirstSample.java
@@ -0,0 +1,56 @@
+package com.su.basics;
+
+/**
+ * 打印语句
+ * 打印并换行
+ * println-> 获取传入的参数 -> 同步锁 -> 进行print进行循环处理字符 最后进行换行处理
+ * @author
+ */
+public class FirstSample {
+    public static void main(String[] args) {
+        //System.out.println("We will not use 'Hello,world'");
+
+        /**
+         * 数据类型
+         * 整型
+         *
+         * int
+         * short
+         * long
+         * byte
+         *
+         * 浮点型
+         *
+         * float  后缀 f/F
+         * double 后缀 d/D
+         *
+         * 字符型
+         *
+         * char 遵循Unicode编码机制
+         *
+         * 布尔型
+         *
+         * true
+         * false
+         */
+        //变量的声明：  变量类型 变量名
+        //变量命名规则： 字母开头并由字母还在数字组成的  不能是java保留字作为命名字段 变量的命名要有可读性
+        //变量的初始化:  使用变量时 要先对变量初始化
+        /*例-错误例子:
+        int vacationDays;
+        System.out.println(vacationDays);  //Variable 'vacationDays' might not have been initialized
+        */
+
+        /*例-变量赋值
+        int vacationDays;
+        vacationDays=12;
+        System.out.println(vacationDays);
+        */
+
+
+
+
+    }
+
+
+}
